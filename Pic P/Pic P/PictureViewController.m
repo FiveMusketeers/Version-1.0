c048b5b70bce655c4fce6c570129fe592832b70b
Steven Wong <sgwong@sfu.ca>
2012-10-18 16:21:19 -0700

    Sub-Menu Views

---
+++ Pic P/Pic P/PictureViewController.m
@@ -0,0 +1,44 @@
+//
+//  PictureViewController.m
+//  MakeTabbed3
+//
+//  Created by Steven Wong on 10/18/12.
+//  Copyright (c) 2012 Steven Wong. All rights reserved.
+//
+
+#import "PictureViewController.h"
+
+@interface PictureViewController ()
+
+@end
+
+@implementation PictureViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
