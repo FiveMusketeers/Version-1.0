f9702ac10926fa44fca07dacddb1d1d8e90615c0
Steven Wong <sgwong@wmfmac20.ucslabs.sfu.ca>
2012-10-19 15:24:39 -0700

    Forgot stuff to add in

---
+++ Pic P/Pic P/ShareCategoriesViewController.m
@@ -0,0 +1,46 @@
+//
+//  ShareCategoriesViewController.m
+//  Pic P
+//
+//  Created by Charlotte Chak on 10/18/12.
+//  Copyright (c) 2012 Five Musketeers. All rights reserved.
+//
+
+#import "ShareCategoriesViewController.h"
+
+@interface ShareCategoriesViewController ()
+
+@end
+
+@implementation ShareCategoriesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"Share", @"First");
+        self.tabBarItem.image = [UIImage imageNamed:@"First"];
+    }
+    return self;
+
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
