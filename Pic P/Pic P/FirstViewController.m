0ef1382c30a397b159c0a3123cef981cd5dea524
Steven Wong <sgwong@wmfmac20.ucslabs.sfu.ca>
2012-10-19 15:51:37 -0700

    Crashing Button

--- Pic P/Pic P/FirstViewController.m
+++ Pic P/Pic P/FirstViewController.m
@@ -52,7 +52,7 @@
 
 - (IBAction)manageList{
     // Should go to the second view (List View)
-    SecondViewController *secondView = [[SecondViewController alloc]initWithNibName:nil bundle:nil];
+    SecondViewController *secondView = [[SecondViewController alloc]initWithNibName:@"SecondViewController" bundle:nil];
     [self presentModalViewController:secondView animated:YES];
     
 }
