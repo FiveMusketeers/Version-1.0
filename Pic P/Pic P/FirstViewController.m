a252a035cf46988fb67b22638372f642545f4703
Steven Wong <sgwong@wmfmac15.ucslabs.sfu.ca>
2012-11-06 12:51:06 -0800

    Changed the way the pictures are

--- Pic P/Pic P/FirstViewController.m
+++ Pic P/Pic P/FirstViewController.m
@@ -18,21 +18,10 @@
 
 
 @implementation FirstViewController
-
-@synthesize img_list, img_category, img_picture, img_share;
-
 							
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    UIImage *menu1_item1 = [UIImage imageNamed:(@"List.png")];
-	[img_list setImage: menu1_item1];
-    UIImage *menu1_item2 = [UIImage imageNamed:(@"Category.png")];
-	[img_category setImage: menu1_item2];
-    UIImage *menu1_item3 = [UIImage imageNamed:(@"Picture.png")];
-	[img_picture setImage: menu1_item3];
-    UIImage *menu1_item4 = [UIImage imageNamed:(@"Share.png")];
-	[img_share setImage: menu1_item4];
 }
 
 - (void)viewDidUnload
